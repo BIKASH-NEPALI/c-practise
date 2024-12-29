f1ddd71864c500cdf184f4da5c3577ef829ff72f
BIKASH-NEPALI <nepalibikash225@gmail.com>
2024-12-29 20:27:52 +0545

    my second code

---
+++ question1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("Enter the value of a:");
+    scanf("%d", &a);
+    printf("Enter the value of b:");
+    scanf("%d", &b);
+    int sum =a+b;
+    printf("The sum of two numbers is %d", sum);
+    return 0;
+}
